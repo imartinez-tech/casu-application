c6b78b93718567b2e1c563ff408b706e9cd5f083
Joshua Harris <Justjoshua1292@gmail.com>
2021-09-16 21:33:37 -0400

    added table & created new routes for home page after login-routes work for racing and rockpaperscissors

--- public/leaderboard.js
+++ public/leaderboard.js
@@ -1,3 +1,21 @@
+var highscores = JSON.parse(localStorage.getItem("highscores"));
+var scorelistol = document.getElementById("scoring");
+
+for (let index = 0; index < highscores.length; index++) {
+    var newli = document.createElement("li")
+    newli.textContent=highscores[index].initals+" - " + highscores[index].score
+    scorelistol.appendChild(newli)
+}
+
+function clearHighscores () {
+    localStorage.clear ();
+    location.reload ();
+}
+
+var clearButton = document.getElementById("clearBtn");
+clearButton.addEventListener("click", clearHighscores);
+
+
 function saveScores () {
     var initals = initalsEl.value.toUpperCase ();
     if (initals === "") {
@@ -43,5 +61,5 @@ startBtn.onclick = startQuiz;
 
 
 
-startBtn.addEventListener("click", startQuiz) 
-initalsEl.onkeyup = checkForEnter;
+// startBtn.addEventListener("click", startQuiz) 
+// initalsEl.onkeyup = checkForEnter;
