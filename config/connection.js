27bec7d4aba654075f3a2d006f15fead0d6d5436
edna galindo <ednagalindo@ednas-mbp.lan>
2021-09-18 08:34:11 -0400

    db information

--- config/connection.js
+++ config/connection.js
@@ -5,17 +5,19 @@ let sequelize;
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize('mysql://b99b66a567cdaa:67906a71@us-cdbr-east-04.cleardb.com/heroku_053dc4caf001f3d?reconnect=true');
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'localhost',
+      host: 'us-cdbr-east-04.cleardb.com',
       dialect: 'mysql',
       port: 3306
     }
   );
+  sequelize = new Sequelize('mysql://b99b66a567cdaa:67906a71@us-cdbr-east-04.cleardb.com/heroku_053dc4caf001f3d?reconnect=true');
 }
 
-module.exports = sequelize;
+module.exports = sequelize
